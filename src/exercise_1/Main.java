20181f4bca3457322a2ddc65b2d9492684d72b7e
KruhliakDmytro <d.noharris@gmail.com>
2019-03-10 21:10:58 +0200

    1st exercise done///

---
+++ src/exercise_1/Main.java
@@ -0,0 +1,28 @@
+package exercise_1;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String resultString;
+        String enteredString = "Holla is a String with Spaces and UPPERCases";
+        String replaceSpace = enteredString.replaceAll(" ", "");
+        String[] splitedEnteredString = replaceSpace.split("");
+        int numbersOfWords = rnd(2, replaceSpace.length() / 5);
+        System.out.println(enteredString);
+        for (int i = 0; i < numbersOfWords; i++) {
+
+            resultString = "";
+
+            for (int j = 0; j < rnd(3, 5); j++) {
+                resultString += splitedEnteredString[rnd(0, replaceSpace.length() - 1)];
+            }
+
+            System.out.println(resultString.toLowerCase());
+        }
+    }
+
+    private static int rnd(int low, int high) {
+        return (int) (Math.random() * (high - low + 1)) + low;
+    }
+
+}
