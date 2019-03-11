3bed594d92777e810ae74b6d773ff2934beca25e
KruhliakDmytro <d.noharris@gmail.com>
2019-03-11 23:39:53 +0200

    3-rd exercise is Done!

--- src/exercise_3/Main.java
+++ src/exercise_3/Main.java
@@ -5,12 +5,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String enteredString = scanner.nextLine();
-        String deleteSpaces = enteredString.replaceAll(" ", "");
-        String[] SplitedBySpace = deleteSpaces.split("");
+        String enteredString = scanner.nextLine(); // считал строку, любую, с пробелами
+        String deleteSpaces = enteredString.replace(" ", ""); // удалил все пробелы
+        String[] SplitedBySpace = deleteSpaces.split(""); // Запихнул строку без пробелов в массив
+        String reversedWordStr = "";
 
-        String[] reversedWord = new String[SplitedBySpace.length];
+        for (int i = SplitedBySpace.length; i != 0; i--) {
+            reversedWordStr += SplitedBySpace[i - 1];
+        }
 
-
+        System.out.println((deleteSpaces.equalsIgnoreCase(reversedWordStr))
+                ? "Ваша строка является Палиндромом"
+                : "Ваша строка не является Палиндромом");
     }
 }
