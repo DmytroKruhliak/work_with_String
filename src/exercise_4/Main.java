1aff976000b04a58efefb10f020ae841a4566feb
KruhliakDmytro <d.noharris@gmail.com>
2019-03-17 01:15:14 +0200

    4-ur exercise is almost done, have some trouble with a spaces in array, there are too much. Need to find a way to remove spaces from Array of String.

--- src/exercise_4/Main.java
+++ src/exercise_4/Main.java
@@ -1,7 +1,36 @@
 package exercise_4;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+//        Scanner scan1 = new Scanner(System.in);
+//        String enteredString = scan1.nextLine();
+        String enteredStr = "test0-344 fjlsdfj 999 sdf33 d8 h4 s3";
 
+        char[] charArrayOfEnteredString = enteredStr.toCharArray();
+        char[] semiResultArray = new char[enteredStr.length()];
+
+        String charArrToStr = "";
+
+        String[] strToArrayStr = new String[charArrToStr.length()];
+
+        int i = 0, j = 0;
+
+        for (char t : charArrayOfEnteredString) {
+
+            if (t >= '0' && t <= '9') {
+                semiResultArray[j] = t;
+                j++;
+            }else{
+                j++;
+                semiResultArray[j] = ' ';
+            }
+        }
+        charArrToStr = charArrToStr.copyValueOf(semiResultArray);
+        strToArrayStr = charArrToStr.split(" ");
+        System.out.println(Arrays.toString(strToArrayStr));
     }
+
 }
