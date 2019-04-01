b907097d992e9d9bb2a00fb66022cbbc8bb32a23
Kruhliak_Dmytro <46688347+KruhliakDmytro@users.noreply.github.com>
2019-04-01 18:22:03 +0300

    4 is done!

--- src/exercise_4/Main.java
+++ src/exercise_4/Main.java
@@ -5,45 +5,52 @@
          Например, если дана строка "data 48 call 9 read13 blank0a", то в массиве должны оказаться числа 48, 9, 13 и 0.
 */
 
-        package exercise_4;
-
+package exercise_4;
+//test0-34##4s№;%s fjlsdfjs 9+9-9 sdf33 d8 h4 s3
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        Scanner scan1 = new Scanner(System.in);
-//        String enteredString = scan1.nextLine();
-        String enteredStr = "test0-344ss fjlsdfjs 99-9 sdf33 d8 h4 s3";
+        Scanner scan1 = new Scanner(System.in);
+        String enteredString = scan1.nextLine();
+        System.out.println(Arrays.toString(getNumbers(enteredString)));
+    }
 
-        char[] charArrayOfEnteredString = enteredStr.toCharArray();
-        char[] semiResultArray = new char[enteredStr.length()];
+    private static int[] getNumbers(String string) {
+        char[] charArr = string.toCharArray();
+        String resStr = "";
+        boolean switcher = false;
 
-        String charArrToStr = "";
-
-        String[] strToArrayStr;
-
-        int i = 0, j = 0;
-
-        for (char t : charArrayOfEnteredString) {
-
-            if (t >= '0' && t <= '9') {
-                semiResultArray[j] = t;
-                j++;
-            }else{
-                j++;
-                semiResultArray[j] = ' ';
+        for (char c : charArr) {
+            if (mainCondition(c)) {
+                resStr += c;
+                switcher = true;
+            } else {
+                if (switcher) {
+                    resStr += "-";
+                }
+                switcher = false;
             }
         }
+        return split(resStr, "-");
 
-        charArrToStr = String.copyValueOf(semiResultArray);
-        strToArrayStr = charArrToStr.split(" ");
-//        for (char t :
-//                semiResultArray) {
-//            if (t != ' ') {
-//                semiResultArray[i++] = t;
-//            }
-//        }
-        System.out.println((Arrays.toString(strToArrayStr)));
+    }
+
+    private static int[] split(String str, String div) {
+        String[] strArr = str.split(div);
+        int i = 0;
+        int[] intArrRes = new int[str.length()];
+        for (String t :
+                strArr) {
+            intArrRes[i++] = Integer.parseInt(t);
+        }
+        return intArrRes;
+    }
+
+
+    private static boolean mainCondition(char ch) {
+        return ('0' <= ch) && (ch <= '9');
     }
 
 }
